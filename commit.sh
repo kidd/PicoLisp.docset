f4fd19ad8544e059c2b8548bce573ea6f597d9be
Raimon Grau <rgrau@ravenpack.com>
2017-01-16 21:14:36 +0100

    updated commit message

--- commit.sh
+++ commit.sh
@@ -1,2 +1,3 @@
-v="This document describes PicoLisp $(head -1 picoLisp/README)"
-git commit -am "$v"
+v="This document describes PicoLisp $(ls picoLisp*tgz |sed -e 's/.tgz//')"
+#git commit -am "$v"
+echo $v
